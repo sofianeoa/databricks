4b897029f6433ef915b12a941768d9d09b5443a3
sofianeoa <sofianeoa>
2023-11-09 14:14:05 +0000

    MAJ

--- A - Initiaisation groupe 2.py
+++ A - Initiaisation groupe 2.py
@@ -16,56 +16,70 @@ except Exception as e:
 
 # COMMAND ----------
 
-import pandas as pd
-import sqlite3
-import re
-
-# Fonction pour nettoyer les noms de colonnes
-def clean_column_names(column_name):
-    # Utiliser une expression régulière pour remplacer les caractères non valides
-    return re.sub(r'[ ,;{}()\n\t=]+', '_', column_name)
-
-# Nom de la base de données SQLite
-db_name = 'my_dataset.db'
-
-# Créer ou se connecter à la base de données SQLite
-conn = sqlite3.connect(db_name)
-
-# Liste des fichiers CSV
-csv_files = ['/mnt/mntgrp2/test.csv', '/mnt/mntgrp2/train.csv', '/mnt/mntgrp2/val.csv']
-
-
-# COMMAND ----------
-
-# Parcourir la liste des fichiers CSV
-for file_path in csv_files:
-    # Lire le fichier CSV dans un DataFrame pandas
-    df = pd.read_csv(file_path)
-    
-    # Nettoyer les noms des colonnes
-    df.rename(columns=clean_column_names, inplace=True)
-    
-    # Déduire le nom de la table à partir du nom du fichier
-    table_name = re.sub(r'.csv$', '', file_path.split('/')[-1])
-    
-    # Enregistrer le DataFrame dans la base de données SQLite comme table
-    df.to_sql(table_name, conn, if_exists='replace', index=False)
-
-# Fermer la connexion à la base de données
-conn.close()
-
-# COMMAND ----------
-
 files = dbutils.fs.ls("/mnt/mntgrp2")
 for file in files:
-    print(file.name, file.size)
-file_list = dbutils.fs.ls("/mnt/mntgrp2")
-
-
-for file in file_list:
     # Vérifiez si le fichier est un fichier CSV par exemple
     if file.name.endswith('.csv'):
         # Lecture du fichier CSV dans un DataFrame Spark
         df = spark.read.option("header", "true").csv(file.path)
 
+        # Enregistrement du DataFrame en tant que table
+        df.write.table(f"dbfs:/mnt/mntgrp2/{file.name[:-4]}_tbl")
+
+        # Affichage du DataFrame
         display(df)
+
+        # Changement des noms des colonnes
+        new_cols = []
+        for col in df.columns:
+            new_col = col.replaceAll("[ ,;{}()\n\t=]+", "")
+            new_cols.append(new_col)
+
+        # Mise à jour des noms des colonnes
+        df.columns = new_cols
+
+        # Affichage du DataFrame avec les nouveaux noms de colonnes
+        display(df)
+
+# COMMAND ----------
+
+from pyspark.sql import SparkSession
+
+
+file_list = dbutils.fs.ls("/mnt/mntgrp2")
+
+for file in file_list:
+    if file.name.endswith('.csv'):
+        # Lecture du fichier CSV dans un DataFrame Spark
+        df = spark.read.option("header", "true").csv(file.path)
+
+        # Déduire le nom de la table à partir du nom du fichier
+        table_name = file.name.replace('.csv', '')
+
+        # Enregistrer le DataFrame en tant que table
+        df.write.format("parquet").saveAsTable(table_name)
+
+spark.stop()
+
+# COMMAND ----------
+
+file_list = dbutils.fs.ls("/mnt/mntgrp2")
+
+for file in file_list:
+    if file.name.endswith('.csv'):
+        # Déduire le nom de la table à partir du nom du fichier
+        table_name = file.name.replace('.csv', '')
+
+        # Supprimer la table si elle existe
+        spark.sql(f"DROP TABLE IF EXISTS {table_name}")
+
+        # Lecture du fichier CSV dans un DataFrame Spark
+        df = spark.read.option("header", "true").csv(file.path)
+
+        # Enregistrer le DataFrame en tant que table
+        df.write.format("parquet").saveAsTable(table_name)
+
+tables = spark.sql("SHOW TABLES")
+tables.show()
+
+spark.stop()
